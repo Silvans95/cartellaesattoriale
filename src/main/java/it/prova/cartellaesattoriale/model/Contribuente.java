f9b15ea97929b99e39d3522d021dc6c2051b5e24
silvans95 <silvano.paloni@gmail.com>
2021-11-19 14:29:02 +0100

    aggiunti modelli di contribuente, cartella esattoriale e stato. modificate le application

---
+++ src/main/java/it/prova/cartellaesattoriale/model/Contribuente.java
@@ -0,0 +1,116 @@
+package it.prova.cartellaesattoriale.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contribuente")
+public class Contribuente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "cognome")
+	private String cognome;
+
+	@Column(name = "datadinascita")
+	private Date dataDiNascita;
+
+	@Column(name = "codiceFiscale")
+	private String codiceFiscale;
+
+	@Column(name = "indirizzo")
+	private String indirizzo;
+
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "contribuente")
+	private Set<CartellaEsattoriale> cartelle = new HashSet<CartellaEsattoriale>(0);
+
+	public Contribuente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Contribuente(Long id, String nome, String cognome, Date dataDiNascita, String codiceFiscale,
+			String indirizzo, Set<CartellaEsattoriale> cartelle) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataDiNascita = dataDiNascita;
+		this.codiceFiscale = codiceFiscale;
+		this.indirizzo = indirizzo;
+		this.cartelle = cartelle;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public Set<CartellaEsattoriale> getCartelle() {
+		return cartelle;
+	}
+
+	public void setCartelle(Set<CartellaEsattoriale> cartelle) {
+		this.cartelle = cartelle;
+	}
+
+}
